e31ab6b5a18aa5a493ebc8130023ce7aaf398842
pashara <serial-i@ya.ru>
2017-12-08 20:14:48 +0300

    Added source code

---
+++ Laba 6/Device.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_6
+{
+    public class Device
+    {
+
+        private ManagementObject _device;
+        private ManagementObject _driver;
+
+
+        public string DeviceID
+        {
+            get
+            {
+                return asdasd(_device["DeviceID"]);
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                return asdasd(_device["Name"]);
+            }
+        }
+
+        private string asdasd(object a)
+        {
+            if (a != null)
+            {
+                return a.ToString();
+            }
+            return "";
+        }
+
+
+        public string GuID
+        {
+            get
+            {
+                return asdasd(_device["ClassGuid"]);
+            }
+        }
+
+
+
+        public string Hardware
+        {
+            get
+            {
+                var hardware = _device["HardwareID"] as string[];
+                string result = string.Empty;
+                foreach (var temp in hardware)
+                {
+                    if (temp != "(null)")
+                    {
+                        result += temp + "    ";
+                    }
+                }
+                return result;
+            }
+        }
+
+
+        public string Manufacturer
+        {
+            get
+            {
+                return asdasd(_device["Manufacturer"]);
+            }
+        }
+
+        public string DriverDescription
+        {
+            get
+            {
+                if (_driver != null)
+                {
+
+                    return asdasd(_driver["Description"]);
+                }
+                return "";
+            }
+        }
+        public string DriverPath
+        {
+            get
+            {
+                if (_driver != null)
+                {
+                    return asdasd(_driver["PathName"]);
+                }
+                return "";
+            }
+        }
+
+        public bool IsEnable
+        {
+            get
+            {
+                var code = _device["ConfigManagerErrorCode"];
+                return !(Convert.ToInt32(code) == 22);
+            }
+        }
+
+
+
+        public Device(ManagementObject device, ManagementObject driver)
+        {
+            _device = device;
+            _driver = driver;
+        }
+
+        public Device()
+        {
+        }
+
+        public bool Disconnect()
+        {
+            try
+            {
+                _device.InvokeMethod("Disable", null);
+            }
+            catch (ManagementException)
+            {
+                return false;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e);
+            }
+            return true;
+        }
+
+        public bool Connect()
+        {
+            try
+            {
+                _device.InvokeMethod("Enable", null);
+            }
+            catch (ManagementException)
+            {
+                return false;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e);
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Laba 6/MainWindowModelView.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Laba_6
+{
+    public class MainWindowModelView : INotifyPropertyChanged
+    {
+
+
+        private string _programTitle = "Device manager";
+        public string H1Title => "Devices Manager";
+        public string ProgramTitle
+        {
+            get
+            {
+                return _programTitle;
+            }
+            private set
+            {
+                _programTitle = value;
+                OnPropertyChanged(nameof(ProgramTitle));
+            }
+        }
+
+
+
+        DeviceManager manager = new DeviceManager();
+        ObservableCollection<Device> _deviceList = new ObservableCollection<Device>();
+        public ObservableCollection<Device> DevicesList
+        {
+            get
+            {
+                return _deviceList;
+                //return manager.Devices;
+            }
+        }
+
+        Device _selectedDevice = new Device();
+        public int SelectedDeviceIndex
+        {
+            get;
+            set;
+        } 
+        public Device SelectedDevice
+        {
+            get
+            {
+                return _selectedDevice;
+            }
+            set
+            {
+                _selectedDevice = value;
+                OnPropertyChanged(nameof(SelectedDevice));
+            }
+        }
+
+        private void UpdateInfoInAnotherThread()
+        {
+            new Thread(() =>
+            {
+                _deviceList = manager.Devices;
+
+                OnPropertyChanged(nameof(DevicesList));
+                OnPropertyChanged(nameof(SelectedDevice));
+            }).Start();
+        }
+
+        public MainWindowModelView()
+        {
+            MainWindow.OnSelectedDevice += MainWindow_OnSelectedDevice;
+            MainWindow.OnMouseDeviceDoubleClick += MainWindow_OnMouseDeviceDoubleClick;
+
+
+            UpdateInfoInAnotherThread();
+        }
+
+        private void MainWindow_OnSelectedDevice(Device device)
+        {
+            SelectedDevice = device;
+        }
+
+        private void MainWindow_OnMouseDeviceDoubleClick(Device device)
+        {
+            if (device.IsEnable)
+            {
+                if (!device.Disconnect())
+                {
+                    MessageBox.Show( "Не могу отключить устройство", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Устройсвто отключено","Информация",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+                    UpdateInfoInAnotherThread();
+                }
+            }
+            else
+            {
+
+                if (!device.Connect())
+                {
+                    MessageBox.Show("Не могу включить устройство", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show( "Устройсвто включено", "Информация",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+                    UpdateInfoInAnotherThread();
+                }
+            }
+
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
